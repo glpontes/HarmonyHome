26f00540d9bae3170ee60c1d758b46f3b59a0239
glpontes <gabriel.pontes@dcx.ufpb.br>
2024-08-28 15:49:10 -0300

    inicial commit

---
+++ src/main/java/com/gr/HarmonyHomeApplication.java
@@ -0,0 +1,13 @@
+package com.gr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonyHomeApplication.class, args);
+	}
+
+}
