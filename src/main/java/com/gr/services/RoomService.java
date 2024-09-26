cd52789517875ea9a39fda061d329c2bc62430ec
Leandro Pontes Da Silva <gleandrop@minsait.com>
2024-09-26 09:56:02 -0300

    feito relacao Houses e Rooms

--- src/main/java/com/gr/services/RoomService.java
+++ src/main/java/com/gr/services/RoomService.java
@@ -5,17 +5,22 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.gr.entity.House;
 import com.gr.entity.Room;
 import com.gr.exception.RoomNotFoundException;
+import com.gr.exception.UserNotFoundException;
+import com.gr.repository.HouseRepository;
 import com.gr.repository.RoomRepository;
 
 @Service
 public class RoomService {
 
 	private RoomRepository roomRepository;
+	private HouseRepository houseRepository;
 
-    public RoomService(RoomRepository roomRepository) {
+    public RoomService(RoomRepository roomRepository, HouseRepository houseRepository) {
         this.roomRepository = roomRepository;
+        this.houseRepository = houseRepository;
     }
 
     public List<Room> listRooms() {
@@ -26,15 +31,20 @@ public class RoomService {
         return roomRepository.findById(roomId).orElseThrow(() -> new RoomNotFoundException("Room not found"));
     }
 
-    public Room createRoom(Room room) {
-        return roomRepository.save(room);
+    public Room createRoom(Room room, Long houseId) {
+        Optional<House> houseOptional = houseRepository.findById(houseId);
+        if(houseOptional.isPresent()) {
+        	room.setHouse(houseOptional.get());
+        	return roomRepository.save(room);
+        }
+        throw new RoomNotFoundException("Room not found");
     }
 
     public Room updateRoom(Long roomId, Room room) {
         Optional<Room> roomOptional = roomRepository.findById(roomId);
         if(roomOptional.isPresent()) {
             Room roomToUpdate = roomOptional.get();
-            roomToUpdate.setName(room.getName());
+            roomToUpdate.setName(room.getName()); 
             return roomRepository.save(roomToUpdate);
         }
         throw new RoomNotFoundException("Room not found");
